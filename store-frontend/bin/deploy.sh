4244a7868b77229a2fc30f7d2d304df8c3f0d06f
unknown <hussain.qurain@outlook.com>
2023-05-02 19:41:10 +0300

    added deploy and tests

---
+++ store-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./dist/my-store s3://elasticbeanstalk-us-east-1-250964198472/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/my-store/index.html s3://elasticbeanstalk-us-east-1-250964198472/
