7eff2cd61220a3ae059631c31e969963cda64b08
unknown <hussain.qurain@outlook.com>
2023-05-02 10:52:45 +0300

    adjusted the process.env consts

--- store-backend/src/database.ts
+++ store-backend/src/database.ts
@@ -3,14 +3,13 @@ import { Pool } from 'pg'
 
 dotenv.config()
 
-const {
-  POSTGRES_HOST,
-  POSTGRES_DB,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_TEST_DB,
-  ENV,
-} = process.env
+const ENV = process.env.ENV;
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_TEST_DB = process.env.POSTGRES_TEST_DB;
+
 
 let client
 //this is to see if my ENV variable has extra space or not.
